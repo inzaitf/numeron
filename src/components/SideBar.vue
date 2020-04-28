fbfad0ff307a325435612552f3e0e414281c2313
白尾　颯真 <soma@somanoMacBook-Air.local>
2020-04-28 18:56:24 +0900

    ヘッダーとサイドバーの作成

---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row class="my-8">
+    <router-link :to="link" class="link-black font-weight-bold">
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon size="50px" left>{{ icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title style="font-size: 25px;">{{
+          menu
+        }}</v-list-item-title>
+      </v-list-item>
+    </router-link>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "side-bar",
+  props: ["menu", "icon", "link"]
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <v-app>
-
-      <!-- 説明画面 -->
-    <v-container style="background-image: url('https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg');" fluid>
+    <!-- 説明画面 -->
+    <v-container
+      style="background-image: url('https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg');"
+      fluid
+    >
       <v-row align="center" justify="center" no-gatters>
-        <v-col cols='12' xs="3">
+        <v-col cols="12" xs="3">
           <v-row align="center" justify="center">
             ルール
           </v-row>
         </v-col>
       </v-row>
-      <v-row align="center" justify="center" >
-        <v-col cols='12' xs="3">
+      <v-row align="center" justify="center">
+        <v-col cols="12" xs="3">
           <v-row align="center" justify="center">
             遊ぶモード
           </v-row>
@@ -19,63 +21,17 @@
       </v-row>
     </v-container>
 
-      <!-- モード選択画面 -->
+    <!-- モード選択画面 -->
     <v-container style="height: 100% ;" fluid>
-      <v-row align="center" justify="center" style="height: 100%;">
-        <v-col cols="12" style="height: 100%;">
-          <v-row align="center" justify="center" ma-5 >
-            <!-- VSCOMモード -->
-
-            <v-col cols='12' xs="3">
-              <v-row align="center" justify="center">
-                <mode-btn
-                  btn_color="light-blue lighten-1"
-                  mode_name="VSCOM"
-                  link_name="/vscom"
-                >
-                </mode-btn>
-              </v-row>
-            </v-col>
-            <!-- ONLINEモード -->
-            <v-col cols='12' xs="3">
-              <v-row align="center" justify="center">
-                <mode-btn
-                  btn_color="light-blue lighten-1"
-                  mode_name="ONLINE"
-                  link_name="/online"
-                >
-                </mode-btn>
-              </v-row>
-            </v-col>
-            <!-- JUDGEモード -->
-            <!-- <v-col cols='12' xs="3">
-              <v-row align="center" justify="center"> -->
-                <mode-btn
-                  btn_color="light-blue lighten-1"
-                  mode_name="JUDGE"
-                  link_name="/judge"
-                >
-                </mode-btn>
-              <!-- </v-row>
-            </v-col> -->
-          </v-row>
-        </v-col>
-      </v-row>
+      <router-view></router-view>
     </v-container>
-    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import ModeBtn from '@/components/ModeBtn.vue'
-
 export default {
+  name: "Home",
 
-  name: 'Home',
-
-  components: {
-    ModeBtn
-  },
-
+  components: {}
 };
 </script>
